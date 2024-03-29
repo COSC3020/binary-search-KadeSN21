9661c5370d0bced63aa2238e92c1525b08d2e3d2
Kade Neville <116524466+KadeSN21@users.noreply.github.com>
2024-03-29 01:18:46 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,19 @@
 function binarySearch(list, element) {
-    for(var i = 0; i < list.length; i++){
-        if (list[i] == element)
-            return i;
+    let left = 0;
+    let right = list.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (list[mid] === element) {
+            return mid; 
+        } else if (list[mid] < element) {
+            left = mid + 1; 
+        } else {
+            right = mid - 1; 
+        }
     }
-    return -1;
+
+    return -1; 
 }
+
