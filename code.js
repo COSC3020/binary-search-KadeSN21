9fc5e5694c5096afb8e1b067da0e82977b9b2084
Kade Neville <116524466+KadeSN21@users.noreply.github.com>
2024-03-29 01:31:05 -0600

    Update code.js

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ function binarySearch(list, element) {
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
 
-        if (list[mid] === element) {
+        if (list[mid] === element && (mid === 0 || list[mid - 1] !== element)) {
             return mid; 
         } else if (list[mid] < element) {
             left = mid + 1; 
@@ -14,6 +14,10 @@ function binarySearch(list, element) {
         }
     }
 
-    return -1; 
+    return -1
 }
 
+const array = [18,18,18];
+const target = 18;
+const index = binarySearch(array, target);
+console.log(index); // Output: 0 (index of the first occurrence of 1)
